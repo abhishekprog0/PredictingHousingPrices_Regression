4fb6473c4d9987dbb86788281fcc2e58ed48b798
Abhishek Mukherjee <abhishekmukherjee1408@gmail.com>
2016-12-08 01:09:26 +0530

    Update Predicting House Prices (Multivariate Linear Regression).cpp

--- Predicting House Prices (Multivariate Linear Regression).cpp
+++ Predicting House Prices (Multivariate Linear Regression).cpp
@@ -12,15 +12,33 @@ int main()
 	r(i,0,m)
 		r(j,1,n+2)
 			cin>>in[i][j];
+	//initializing feature no. 0 to 0  
 	r(i,0,m)
-		in[i][0]=1;		
+		in[i][0]=1;	
+	//initializing parameter vector
 	r(i,0,n+1)
-		p[i]=-1.1;	
+		p[i]=-1.1;
+	//mean normalization
+	r(i,1,n+1)
+	{
+		s0=0;
+		min = max = in[0][i];
+		r(j,0,m)
+		{
+			s0 +=in[j][i];
+			if(in[j][i] < min ) 
+				min = in[j][i];
+			if(in[j][i]>max)
+				max=in[j][i];		
+		}
+		mean = s0/m;
+		range = max - min;
+		r(j,0,m)
+			in[j][i] = (in[j][i] - mean)/range;
+	}	
 	float s1=0,s2=0;
 	float alpha = 0.1; //alpha = learning rate		
 	//linear regression using gradient descent
-	//cout<< "Iterations for parameters: "<<endl;
-	//cout<<"m         c"<<endl;		
 	while(l<100)
 	{
 		r(k,0,n+1)
