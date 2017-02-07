6cacb88450c47f14fea8d99a5bd57dcf58cb0ffa
Abhishek Mukherjee <abhishekmukherjee1408@gmail.com>
2017-02-08 00:25:08 +0530

    Update Simple Linear Regression.cpp

--- Simple Linear Regression.cpp
+++ Simple Linear Regression.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
+float abs(float x, float y)
+{
+	if( x > y)
+		return (x - y);
+	return (y - x);
+}
 int main()
 {
 	 /* The following code implements Linear Regression with one Feature Variable */
@@ -8,7 +14,7 @@ int main()
 	cin >> m;
 	// Used array to store the number of training data for feature_1.
 	float feature_1[m];
-	float target_output[m], predicted_output;
+	float target_output[m], predicted_output, pred;
 	//input feature 1
 	for(int i = 0; i < m; i++) 
 		cin  >> feature_1[i];
@@ -17,14 +23,14 @@ int main()
 		cin >> target_output[i];
 	//y = k1 * feature_1 + k2;
 	//k1 and k2 represents the parameters of line y
-	float k1 = -1,k2 = 2; //Initial Guess
-	float s1 = 0,s2 = 0;
-	float p1 = 1,p2 = 1;
+	float k1 = -1, k2 = 2; //Initial Guess
+	float s1 = 0, s2 = 0;
+	float p1 = 1, p2 = 1;
 	float alpha = 0.01; //alpha = learning rate		
 	//linear regression using gradient descent
 	cout<< "Iterations for parameters: "<<endl;
 	//cout<<"m         c"<<endl;		
-	while(abs(p2-k2)>=0.000001)
+	while(abs(p2 - k2) >= 0.000001)
 	{
 		p1 = k1;
 		p2 = k2;
@@ -37,12 +43,12 @@ int main()
 		s2 = s2 / m;
 		k2 = k2 - ( alpha * s2 );
 		k1 = k1 - ( alpha * s1 );
-		cout<<k1<<" "<<k2<<endl;
+		cout << k1 << " " << k2 << endl;
 	}
-	cout<<"m = "<<k1<<", c = "<<k2<<endl;
-	cout<<"Enter your prediction input variable: ";
-	cin>>pred;
+	cout << "m = " << k1 << ", c = " << k2 << endl;
+	cout << "Enter your prediction input variable: ";
+	cin >> pred;
 	float final = k2 * pred + k1;
-	cout<<"Predicted output: "<<final;			
+	cout << "Predicted output: " << final;			
 }
 
